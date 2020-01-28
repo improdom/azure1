4b4adbab612c25612ac103dc6d3368ada524b34c
Julio <juniordiaz@hotmail.com>
2020-01-27 20:52:34 -0500

    test

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace azure1
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!!!");
+                    await context.Response.WriteAsync("Hello World from Improdom!!!");
                 });
             });
         }
